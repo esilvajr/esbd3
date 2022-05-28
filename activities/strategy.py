9eaf3f216cd00411a162de6c380af996be075f83
Edison Silva Jr <esilvajr@hotmail.com>
2022-05-27 21:19:42 -0300

    Alterando o exemplo de strategy para a logica condicional no __main__

--- activities/strategy.py
+++ activities/strategy.py
@@ -1,29 +1,26 @@
 import random
 
 class RouteStrategy():
-    def getRoute() -> str:
+    def getRoute() -> bool:
         raise NotImplementedError
 
 class NotSafeRoute(RouteStrategy):
-    def isSafe(self) -> bool:
-        return random.choice([True, False])
 
-    def getRoute(self) -> str:
-        return "The shortest route is safe by now"
+    def getRoute(self) -> bool:
+        #Pegando comportamento da NotSaferoute
+        return random.choice([True, False])
 
 class SafeRoute(RouteStrategy):
-    def isSafe(self) -> bool:
-        return random.choice([True, False])
 
-    def getRoute(self) -> str:
-        return "The safe route is safe by now"
+    def getRoute(self) -> bool:
+        #Pegando comportamento da SafeRoute
+        return random.choice([True, False])
     
 class SuperSafeRoute(RouteStrategy):
-    def isSafe(self) -> bool:
-        return True
 
-    def getRoute(self) -> str:
-        return "The super safe route is the best one"
+    def getRoute(self) -> bool:
+        #Pegando comportamento da SuperSafeRoute
+        return True
 
 class Route():
     _strategy: None
@@ -34,28 +31,29 @@ class Route():
     def getStrategy(self) -> RouteStrategy:
         return self._strategy
 
-    def calculateRoute(self) -> None:
-
-        if (self._strategy.isSafe() == True):
-            print(self._strategy.getRoute())
-
-        elif(self._strategy.isSafe() == False):
-
-            self._strategy = SafeRoute()
-            if (self._strategy.isSafe() == True):
-                print(self._strategy.getRoute())
-
-            else:
-                self._strategy = SuperSafeRoute()
-                print(self._strategy.getRoute())
+    def calculateRoute(self) -> bool:
+        return self._strategy.getRoute()
 
 
 if __name__ == "__main__":
 
-    route = Route(NotSafeRoute())
-    route.calculateRoute()
+    routeA = Route(NotSafeRoute())
+    routeB = Route(SafeRoute())
+    routeC = Route(SuperSafeRoute())
 
-    print(route.getStrategy())
+    if (routeA.calculateRoute()):
+        print("Rota mais curta escolhida")
+
+    elif(routeB.calculateRoute()):
+        print("Rota média porém segura escolhida")   
+
+    else:
+        print("Rota mais longa porém segura escolhida")
+            
+        
+
+
+
     
 
 
